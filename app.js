b9c2e09e2749280e2abb38abc1fee45a1eaf7004
ismaila sylla <syldox@gmail.com>
2017-10-27 01:36:43 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Woking fine !');
+})
+
+
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+    console.log(` Server started on port ${port} `);
+});
