9056f190c0ed1775ac3cfbe43868cd30f393607a
ismaila sylla <syldox@gmail.com>
2017-10-27 02:20:18 +0530

    creating-our-google-strategy

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const passport = require('passport');
+
+//passport config 
+require('./config/passport')(passport);
+
+
+//load routes 
+const auth = require('./routes/auth');
 
 const app = express();
 
 app.get('/', (req, res) => {
     res.send('Woking fine !');
-})
+});
 
-
+//Use Routes
+app.use('/auth', auth);
 
 const port = process.env.PORT || 5000;
 
---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoURI: 'mongodb://isma:American1@ds133358.mlab.com:33358/storybooks-dev',
+    googleClientID: '1086365423392-52qklb9dql2ph5p3hnn3ofv0kmfu1oag.apps.googleusercontent.com',
+    googleClientSecret: 'yH1sRwdWERSAk2IUebzjUpH5'
+}
