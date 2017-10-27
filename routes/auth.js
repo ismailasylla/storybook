2238985104d6c6dffb1adf76d2adb4764596630d
ismaila sylla <syldox@gmail.com>
2017-10-27 14:25:10 +0530

    handled auth

--- routes/auth.js
+++ routes/auth.js
@@ -3,9 +3,14 @@ const router = express.Router();
 const passport = require('passport');
 
 
-
 router.get('/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
 
 
+router.get('/google/callback',
+    passport.authenticate('google', { failureRedirect: '/login' }),
+    (req, res) => {
+        // Successful authentication, redirect home.
+        res.redirect('/dashboard');
+    });
 
 module.exports = router;
